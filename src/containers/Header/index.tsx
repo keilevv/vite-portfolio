6e584d0767688ae4264f839e5ab332e0c8073448
Caleb Villalba <calvihe@gmail.com>
2024-03-07 10:07:04 -0500

    some improvements to page banner

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -9,7 +9,9 @@ function HeaderContainer() {
         <div className="header-content">
           <div className="header-text">
             <h1 className="header-title"> Caleb Villalba</h1>
-            <h1 className="header-subtitle"> Fullstack web developer</h1>
+            <h1 className="header-subtitle transition-brown ">
+              Fullstack web developer
+            </h1>
             <h1 className="header-description">
               Making it better every day, what is already already done fine.
             </h1>
@@ -21,6 +23,22 @@ function HeaderContainer() {
               alt="Avatar image"
             />
           </div>
+        </div>
+        <div className="social-icons-container">
+          <i
+            className="fa-brands fa-square-github social-icon transition-brown"
+            onClick={() => window.open("https://github.com/caleb-villalba")}
+          ></i>
+          <i
+            className="fa-brands fa-linkedin social-icon transition-brown"
+            onClick={() =>
+              window.open("https://www.linkedin.com/in/caleb-villalba/")
+            }
+          ></i>
+          <i
+            className="fa-brands fa-square-facebook social-icon transition-brown"
+            onClick={() => window.open("https://www.facebook.com/calvihe/")}
+          ></i>
         </div>
       </div>
     </>
